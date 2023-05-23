8092e49cc77e6163c0744e5fef4f4a9ddd841ceb
maximghgh <95348478+maximghgh@users.noreply.github.com>
2023-05-23 15:44:05 +0400

    task6

--- task6/controllers/login.php
+++ task6/controllers/login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?
     include "db.php";
 
@@ -51,4 +52,59 @@
         }
     }
     header("Location:/index.php");  
+=======
+<?
+    include "db.php";
+
+    $login=$_POST['login'];
+    $password=$_POST['password'];
+    $submit=$_POST['submit'];
+
+
+    $disallow = ['~', '\'', '"', '<', '>', '.', '%'];
+    $login = str_replace($disallow, '', $login);
+    $password = str_replace($disallow, '', $password);
+
+    if($submit){
+        if($login && $password){       
+            $str_login="SELECT * FROM `users` where `login`= '$login' and `password`='$password'";
+            
+            $run_login=mysqli_query($connect,$str_login);
+            $login_users=mysqli_num_rows($run_login);
+            
+                if($login_users == 1){
+                    $out_login=mysqli_fetch_array($run_login);
+
+                    echo $out_login['login'];
+
+                    $_SESSION['users']=array
+                (
+                    'id' => $out_login['id'],
+                    'login' => $out_login['login'],
+                    'password' => $out_login['password'],
+                );
+                }
+                else{
+
+                    $str_regist="INSERT INTO `users`(`login`, `password`, `created_at`) VALUES ('$login','$password', CURRENT_TIMESTAMP)";
+                    $run_regist=mysqli_query($connect,$str_regist);
+
+                    $str_login="SELECT * FROM `users` where `login`= '$login' and `password`='$password'";
+                    $run_login=mysqli_query($connect,$str_login);
+                    $out_login=mysqli_fetch_array($run_login);
+                    
+                    $_SESSION['users']=array
+                (
+                    'id' => $out_login['id'],
+                    'login' => $out_login['login'],
+                    'password' => $out_login['password'],
+                );
+                }
+        }
+        else{
+            $_SESSION['good']="<font class=font color=red>Заполните все поля</font>";
+        }
+    }
+    header("Location:/index.php");  
+>>>>>>> b2fff2ef53915b7745f2c65cbdfeafa52e99c06f
 ?>
--- task6/controllers/status.php
+++ task6/controllers/status.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?
 
     include "db.php";
@@ -74,4 +75,82 @@
         }
     } 
 header("Location:/index.php");
+=======
+<?
+
+    include "db.php";
+    $id_users=$_SESSION['users']['id'];  
+    $id_task=$_GET['id_task'];
+    $status=$_GET['status'];
+
+    if($id_task){
+        if($status==0){
+            $upd_tas="UPDATE `tasks` SET `status`='1' WHERE `user_id`='$id_users'";
+            $run_upd_tas=mysqli_query($connect,$upd_tas);
+            if($run_upd_tas){
+                $_SESSION['good']="<font class=font color=green>Задание выполнено</font>";
+            }
+            else{
+                $_SESSION['good']="<font class=font color=red>Ошибка выполнения</font>";
+            }
+            echo $upd_tas;
+        }
+    elseif($status==1){
+        $upd_tas="UPDATE `tasks` SET `status`='0' WHERE `id`='$id_task'";
+        $run_upd_tas=mysqli_query($connect,$upd_tas);
+            if($run_upd_tas){
+                $_SESSION['good']="<font class=font color=green>Задание окозалось не доделанным</font>";
+            }
+            else{
+                $_SESSION['good']="<font class=font color=red>Ошибка выполнения</font>";
+            }
+    
+    }
+}
+    $id_statusal=$_GET['id_statusal'];
+    
+       
+
+     
+
+    function taskGood($upd_tas,$run_upd_tas,$id_users,$connect)
+    {
+        
+        $upd_tas="UPDATE `tasks` SET `status`='1' WHERE `user_id`='$id_users'";
+        $run_upd_tas=mysqli_query($connect,$upd_tas);
+        if($run_upd_tas){
+            $_SESSION['good']="<font class=font color=green>Все задания выполнены</font>";
+        }
+        else{
+            $_SESSION['good']="<font class=font color=red>Ошибка выполнения</font>";
+        }
+    }
+    
+    function taskError($upd_tas,$run_upd_tas,$id_users,$connect)
+    {
+        $upd_tas="UPDATE `tasks` SET `status`='0' WHERE `user_id`='$id_users'";
+        $run_upd_tas=mysqli_query($connect,$upd_tas);
+        if($run_upd_tas){
+            $_SESSION['good']="<font class=font color=green>Все задания отменены</font>";
+        }
+        else{
+            $_SESSION['good']="<font class=font color=red>Ошибка выполнения</font>";
+        }
+    }
+
+    if($id_statusal)
+    {
+        if($status==0)
+        {
+            taskGood($upd_tas,$run_upd_tas,$id_users,$connect);
+
+        }
+        elseif($status==1)
+        {
+            taskError($upd_tas,$run_upd_tas,$id_users,$connect);
+
+        }
+    } 
+header("Location:/index.php");
+>>>>>>> b2fff2ef53915b7745f2c65cbdfeafa52e99c06f
 ?>
